76b84a1241e42fa7beca954efd5cf28fc452f84d
sgtmt <ainnuen@gmail.com>
2017-01-18 01:23:47 +0900

    first commit

---
+++ math_round/Animetion.swift
@@ -0,0 +1,13 @@
+//
+//  Animetion.swift
+//  math_round
+//
+//  Created by 長沢　遼 on 2017/01/11.
+//  Copyright © 2017年 Sgtmt. All rights reserved.
+//
+
+import UIKit
+
+class Animetion: NSObject {
+
+}
